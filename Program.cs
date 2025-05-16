5c81d435deab33d0d9aeaef50220cc4569ab138c
Mih4n <losevmisha0@gmail.com>
2025-05-16 16:36:57 +0300

    added response compression

--- Program.cs
+++ Program.cs
@@ -3,18 +3,25 @@ using Microsoft.AspNetCore.Server.Kestrel.Core;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.ConfigureKestrel(kestrel =>
-{
-    kestrel.ListenAnyIP(443, listenOptions =>
-        {
-            listenOptions.Protocols = HttpProtocols.Http1AndHttp2AndHttp3;
-            listenOptions.UseHttps(httpsOptions =>
-                {
-                    httpsOptions.UseLettuceEncrypt(kestrel.ApplicationServices);
-                }
-            );
-        }
-    );
-});
+    {
+        kestrel.ListenAnyIP(443, listenOptions =>
+            {
+                listenOptions.Protocols = HttpProtocols.Http1AndHttp2AndHttp3;
+                listenOptions.UseHttps(httpsOptions =>
+                    {
+                        httpsOptions.UseLettuceEncrypt(kestrel.ApplicationServices);
+                    }
+                );
+            }
+        );
+    }
+);
+
+builder.Services.AddResponseCompression(options =>
+    {
+        options.EnableForHttps = true;
+    }
+);
 
 builder.Services.AddLettuceEncrypt();
 
@@ -30,6 +37,7 @@ builder
 var app = builder.Build();
 
 app.UseRouting();
+app.UseResponseCompression();
 app.MapReverseProxy();
 
 app.Run();
